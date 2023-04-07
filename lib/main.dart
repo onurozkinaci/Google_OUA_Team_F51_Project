0ffbcacae4f0aeae843dabca24f365a28b160c54
Onur Ozkinaci <onurozkinaci@gmail.com>
2023-04-07 12:46:16 +0300

    main_page is updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _controller = PageController();
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Info Cards")),
+        body: Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView(
+                  controller: _controller,
+                  children: const [
+                    Card(
+                        child:
+                            Text("Card 1") // Card content for the first page,
+                        ),
+                    Card(
+                        child:
+                            Text("Card 2") // Card content for the second page,
+                        ),
+                    Card(
+                        child:
+                            Text("Card 3") // Card content for the second page,
+                        ),
+                    // Add more cards as needed
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                child: Text('Previous'),
+                onPressed: () {
+                  _controller.previousPage(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
+              ElevatedButton(
+                child: Text('Next'),
+                onPressed: () {
+                  _controller.nextPage(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
